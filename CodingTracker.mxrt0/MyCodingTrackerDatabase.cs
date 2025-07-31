3373088d220bf7e6436d4e377e65348c16c6a3e2
Martin Terziev <m.terziev08@abv.bg>
2025-07-31 15:03:21 +0300

    Codacy prompted using directive removal.

--- CodingTracker.mxrt0/MyCodingTrackerDatabase.cs
+++ CodingTracker.mxrt0/MyCodingTrackerDatabase.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Collections.Specialized;
 using System.Linq;
-using System.Text;
 using Dapper;
 using Microsoft.Data.Sqlite;
 using System.Data.SqlClient;
